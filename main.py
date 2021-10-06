d4860ac115b6b6f9e9821e61f577fef45f17eda8
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2021-10-06 13:42:11 +0300

    Fix absolute path for building

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ import os
 from pathlib import Path
 import sys
 
+from PySide6 import QtCore
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
 import controller
+
+application_path = (
+    sys._MEIPASS
+    if getattr(sys, "frozen", False)
+    else os.path.dirname(os.path.abspath(__file__))
+)
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
 
     # QML ui setup
     engine = QQmlApplicationEngine()
-    engine.load(os.fspath(Path(__file__).resolve().parent / "ui/MainApplication.qml"))
+    mainqml = os.path.join( application_path, "ui/MainApplication.qml" )
+    print( application_path )
+    print( mainqml )
+    engine.load( QtCore.QUrl.fromLocalFile(mainqml) )
 
     ## add Python integrations
     backend = controller.Backend()
